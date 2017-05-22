2738caec426fcdbbf4252cd37e4b071573b27688
unknown <>
2017-05-22 17:41:51 +0800

    更新

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 $('.right li a').click(function(){
   //改变右侧文字链接点击后效果
   $('.right li a').removeAttr('style');
-  $(this).attr('style','border-left: #25a7f6 solid 2px; font-weight:bold; color:#25a7f6'); 
+  $(this).attr('style','border-left: #25a7f6 solid 2px; font-weight:bold; color:#25a7f6');
 
   if($(this).attr('href')!=='#2'){ //判断二级菜单
   	 $('.right li ul').attr('style','display:none');
@@ -9,8 +9,11 @@ $('.right li a').click(function(){
 
   $(this).parent().find('.nav').attr('style','display:block');
 
+  //加载时
+  $('.docs-section').html('<div class="box"><img src="js/skin/default/loading-2.gif" /></div>');
+
   //加载html
   var html = $(this).attr('data-src');
   $('.docs-section').load(html);
-  
+
 });
